e74778d04c7b93949364a4ea853145d2f91ab429
IradDV <104461790+IradDV@users.noreply.github.com>
2024-11-02 22:18:21 -0600

    prototipo

---
+++ src/js/scrpt.js
@@ -0,0 +1,18 @@
+paypal.Buttons({
+    createOrder: function(data, actions) {
+        return actions.order.create({
+            purchase_units: [{
+                amount: {
+                    value: '10.00'  // Cambia el valor al monto deseado
+                }
+            }]
+        });
+    },
+    onApprove: function(data, actions) {
+        return actions.order.capture().then(function(details) {
+            alert('Pago completado por ' + details.payer.name.given_name);
+            // Aquí puedes enviar detalles al servidor para confirmar el pago
+        });
+    }
+}).render('#paypal-button-container');
+
